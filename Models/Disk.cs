7960693bdf8dae597bd0357513387b62b010d2e3
shadichy <shadichy@blisslabs.org>
2025-11-29 13:22:57 +0700

    Move root directory
    Signed-off-by: shadichy <shadichy@blisslabs.org>

--- Models/Disk.cs
+++
@@ -1,9 +0,0 @@
-namespace LinuxInstaller.Models;
-
-public class Disk
-{
-    public string Id { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
-    public long Size { get; set; } // in bytes
-    public bool IsBootable { get; set; }
-}
--- ViewModels/Interfaces/INavigatableViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace LinuxInstaller.ViewModels.Interfaces
-{
-    public interface INavigatableViewModel
-    {
-        bool CanProceed { get; }
-        bool CanGoBack { get; }
-    }
-}
